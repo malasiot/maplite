ba3d6241d906f75deccb28c14cbee1b691503fc3
Sotiris Malassiotis <malasiot@iti.gr>
2016-10-26 12:52:17 +0300

    readme.md

--- src/server/map_server.hpp
+++ src/server/map_server.hpp
@@ -4,23 +4,6 @@
 #include "server.hpp"
 #include "fs_tile_cache.hpp"
 
-// This creates the map server that will provide XYZ tiles and other assets required when displaying a map.
-// The server expects a file name config.xml in one or more of the specified root folders.
-// The server provides two end-point types: assets and tiles
-//
-// assets are general files stored raw or inside an sqlite database,(global assets such as fonts, glyphs, js, css) or map specific assets such as styles
-// http://localhost:<port>/<key>/* (map specific assets such as style file)
-// these are specified using <assets src=<folder or sqlite file> key=<key> />
-// For GPX, KML assets the server supports conversion to GeoJson by appending ?cnv=geojson to the request URL.
-//
-// tiles are vector or raster tiles with end point:
-// http://<host>:<port>/<key>/<z>/<x>/<y>.png
-// the corresponding config is:
-// <tiles type="jpeg2000" src=<jpeg2000 raster file> key=<key> />
-// <tiles type="mapsforge" src=<map file path> key=<key> />
-// all paths are relative to the xml file
-
-
 class MapServer: public http::Server {
 public:
     MapServer(const std::string &config_file, const std::string &host_name, const std::string &ports, const std::string &cache_dir = std::string()) ;
